d0905000563e1ee00edde0c9b67fea22969c4f22
Matt Day <mattsday@gmail.com>
2016-04-26 17:27:03 +0100

    Removed Path pie chart for performance reasons

--- 3PAR_Plugin/src/com/cisco/matday/ucsd/hp3par/reports/AccountReport.java
+++ 3PAR_Plugin/src/com/cisco/matday/ucsd/hp3par/reports/AccountReport.java
@@ -23,7 +23,6 @@ package com.cisco.matday.ucsd.hp3par.reports;
 
 import com.cisco.matday.ucsd.hp3par.constants.HP3ParConstants;
 import com.cisco.matday.ucsd.hp3par.reports.graphs.CPGBarChartReport;
-import com.cisco.matday.ucsd.hp3par.reports.graphs.PathPieChart;
 import com.cisco.matday.ucsd.hp3par.reports.graphs.UsagePieChart;
 import com.cisco.matday.ucsd.hp3par.reports.graphs.VolumeSummaryAllocationPieChart;
 import com.cisco.matday.ucsd.hp3par.reports.summary.OverviewTable;
@@ -41,7 +40,6 @@ public class AccountReport extends GenericInfraAccountReport {
 
 	private CloupiaReport[] ddReports = new CloupiaReport[] {
 			new OverviewTable(), new UsagePieChart(), new VolumeSummaryAllocationPieChart(), new CPGBarChartReport(),
-			new PathPieChart()
 	};
 
 	/**
