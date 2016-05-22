6ae150200a7dcaf929cb7c69bda246058092df8b
Matt Day <mattsday@gmail.com>
2016-05-22 14:52:01 +0100

    Initial work on issue #18 (generic selectors)

---
+++ 3PAR_Plugin/src/com/cisco/matday/ucsd/hp3par/inputs/HP3ParVolumeAndVolumeSetSelector.java
@@ -0,0 +1,142 @@
+/*******************************************************************************
+ * Copyright (c) 2016 Matt Day, Cisco and others
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *******************************************************************************/
+package com.cisco.matday.ucsd.hp3par.inputs;
+
+import java.util.List;
+
+import com.cisco.matday.ucsd.hp3par.account.HP3ParCredentials;
+import com.cisco.matday.ucsd.hp3par.account.inventory.HP3ParInventory;
+import com.cisco.matday.ucsd.hp3par.constants.HP3ParConstants;
+import com.cisco.matday.ucsd.hp3par.rest.sets.json.SetResponse;
+import com.cisco.matday.ucsd.hp3par.rest.sets.json.SetResponseMember;
+import com.cisco.matday.ucsd.hp3par.rest.volumes.json.VolumeResponse;
+import com.cisco.matday.ucsd.hp3par.rest.volumes.json.VolumeResponseMember;
+import com.cloupia.fw.objstore.ObjStore;
+import com.cloupia.fw.objstore.ObjStoreHelper;
+import com.cloupia.lib.connector.account.AccountUtil;
+import com.cloupia.lib.connector.account.PhysicalInfraAccount;
+import com.cloupia.model.cIM.InfraAccount;
+import com.cloupia.model.cIM.ReportContext;
+import com.cloupia.model.cIM.TabularReport;
+import com.cloupia.service.cIM.inframgr.TabularReportGeneratorIf;
+import com.cloupia.service.cIM.inframgr.reportengine.ReportRegistryEntry;
+import com.cloupia.service.cIM.inframgr.reports.TabularReportInternalModel;
+
+/**
+ * Table to allow selection of an HP 3PAR volumes - it should not be
+ * instantiated directly but instead used as a form item
+ *
+ * @author Matt Day
+ *
+ */
+public class HP3ParVolumeAndVolumeSetSelector implements TabularReportGeneratorIf {
+
+	@Override
+	public TabularReport getTabularReportReport(ReportRegistryEntry reportEntry, ReportContext context)
+			throws Exception {
+
+		TabularReport report = new TabularReport();
+
+		report.setGeneratedTime(System.currentTimeMillis());
+		report.setReportName(reportEntry.getReportLabel());
+		report.setContext(context);
+
+		ObjStore<InfraAccount> store = ObjStoreHelper.getStore(InfraAccount.class);
+		List<InfraAccount> objs = store.queryAll();
+
+		TabularReportInternalModel model = new TabularReportInternalModel();
+		model.addTextColumn("InternalID", "InternalID", true);
+		model.addTextColumn("ID", "ID");
+		model.addTextColumn("Name", "Name");
+		model.addTextColumn("Type", "Type");
+		model.addTextColumn("Account", "Account");
+		model.addTextColumn("Comment", "Comment");
+
+		model.completedHeader();
+
+		for (InfraAccount a : objs) {
+			PhysicalInfraAccount acc = AccountUtil.getAccountByName(a.getAccountName());
+			// Important to check if the account type is null first
+			if ((acc != null) && (acc.getAccountType() != null)
+					&& (acc.getAccountType().equals(HP3ParConstants.INFRA_ACCOUNT_TYPE))) {
+
+				final HP3ParCredentials credentials = new HP3ParCredentials(a.getAccountName());
+
+				VolumeResponse volumeList = HP3ParInventory
+						.getVolumeResponse(new HP3ParCredentials(a.getAccountName()));
+
+				SetResponse setList = HP3ParInventory.getVolumeSetResponse(new HP3ParCredentials(a.getAccountName()));
+
+				// Internal ID format:
+				// accountName;id@type@name
+
+				for (VolumeResponseMember volume : volumeList.getMembers()) {
+					// Bad but we can use this to parse it all out later
+					String internalId = credentials.getAccountName() + ";" + Integer.toString(volume.getId())
+							+ "@volume@" + volume.getName();
+
+					// Internal ID
+					model.addTextValue(internalId);
+
+					// Volume ID
+					model.addTextValue(Integer.toString(volume.getId()));
+					// Name of this volume
+					model.addTextValue(volume.getName());
+
+					model.addTextValue("Volume");
+
+					// Account Name
+					model.addTextValue(a.getAccountName());
+
+					model.addTextValue(volume.getComment());
+					model.completedRow();
+				}
+
+				for (SetResponseMember volumeSet : setList.getMembers()) {
+					// Internal ID, format:
+					// accountName;volumeid@accountName@volumeName
+					String internalId = credentials.getAccountName() + ";" + Integer.toString(volumeSet.getId())
+							+ "@set@" + volumeSet.getName();
+
+					// Internal ID
+					model.addTextValue(internalId);
+
+					model.addTextValue(Integer.toString(volumeSet.getId()));
+					model.addTextValue(volumeSet.getName());
+
+					model.addTextValue("Volume Set");
+
+					model.addTextValue(a.getAccountName());
+
+					model.addTextValue(volumeSet.getComment());
+
+					model.completedRow();
+				}
+
+			}
+		}
+		model.updateReport(report);
+
+		return report;
+	}
+
+}
